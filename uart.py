7d8274d45447e96ae1c5d5938ae001d21ecc384e
zZz <dinhhongoc@gmail.com>
2024-03-13 18:42:35 +0700

    Create uart.py

---
+++ uart.py
@@ -0,0 +1,70 @@
+import serial.tools.list_ports
+
+ser = None
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+
+isSensorConnected = False
+if getPort() != "None":
+    ser = serial.Serial(port=getPort(), baudrate=115200)
+    isSensorConnected = True
+    print(ser)
+
+def processData(client, data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "T":
+        client.publish("sensor1", splitData[2])
+    elif splitData[1] == "L":
+        client.publish("sensor2", splitData[2])
+    elif splitData[1] == "H":
+        client.publish("sensor3", splitData[2])
+
+mess = ""
+
+def readSerial(client):
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        print(mess)
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(client, mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+
+def writeData(data):
+    print(data)
+    ser.write((str(data)).encode())
+
+
+# RS485 protocol
+def serial_read_data(ser):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        out = ser.read(bytesToRead)
+        data_array = [b for b in out]
+        print(data_array)
+        if len(data_array) >= 7:
+            array_size = len(data_array)
+            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            return value
+        else:
+            return -1
+    return 0
