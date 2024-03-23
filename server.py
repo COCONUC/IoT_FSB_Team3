b312ea3061159816863ba3a47b10865ee0f80387
dinhhn2000 <dinhhongoc@gmail.com>
2024-03-23 11:13:48 +0700

    Update

--- server.py
+++ server.py
@@ -31,14 +31,17 @@ def mqtt_message(client, userdata, message):
             response = setRelay(payload == '1', 1)
             print('Response R1: ' + str(response))
             mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[0], str(response) == '255')
+            print(MQTT_TOPIC_PUB + relay_topics[0])
         case 'r2':
             response = setRelay(payload == '1', 2)
             print('Response R2: ' + str(response))
             mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[1], str(response) == '255')
+            print(MQTT_TOPIC_PUB + relay_topics[2])
         case 'r3':
             response = setRelay(payload == '1', 3)
             print('Response R3: ' + str(response))
             mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[2], str(response) == '255')
+            print(MQTT_TOPIC_PUB + relay_topics[2])
 
 #Inint MQTT client
 mqttClient = mqtt.Client()
