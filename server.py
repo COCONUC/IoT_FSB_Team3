3e92dccba42f245efac95d1f4669394520656e6b
dinhhn2000 <dinhhongoc@gmail.com>
2024-03-23 11:06:30 +0700

    Update

--- server.py
+++ server.py
@@ -30,15 +30,15 @@ def mqtt_message(client, userdata, message):
         case 'r1':
             response = setRelay(payload == '1', 1)
             print('Response R1: ' + str(response))
-            mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[0], str(response) == 255)
+            mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[0], str(response) == '255')
         case 'r2':
             response = setRelay(payload == '1', 2)
             print('Response R2: ' + str(response))
-            mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[1], str(response) == 255)
+            mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[1], str(response) == '255')
         case 'r3':
             response = setRelay(payload == '1', 3)
             print('Response R3: ' + str(response))
-            mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[2], str(response) == 255)
+            mqttClient.publish(MQTT_TOPIC_PUB + relay_topics[2], str(response) == '255')
 
 #Inint MQTT client
 mqttClient = mqtt.Client()
@@ -47,9 +47,20 @@ mqttClient.connect(MQTT_SERVER, int(MQTT_PORT), 60)
 #Register mqtt events
 mqttClient.on_connect = mqtt_connected
 mqttClient.on_subscribe = mqtt_subscribed
-mqttClient.on_message = mqtt_message
+# mqttClient.on_message = mqtt_message
 
 mqttClient.loop_start()
+
+#Inint relays MQTT client
+mqttClientRelays = mqtt.Client()
+mqttClientRelays.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+mqttClientRelays.connect(MQTT_SERVER, int(MQTT_PORT), 60)
+#Register mqtt events
+mqttClientRelays.on_connect = mqtt_connected
+mqttClientRelays.on_subscribe = mqtt_subscribed
+mqttClientRelays.on_message = mqtt_message
+
+mqttClientRelays.loop_start()
 
 while True:
     if isSensorConnected and ser != None:
