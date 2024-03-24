c398940fe6b2da8b5ba5987f96b2ce0316888657
dinhhn2000 <dinhhongoc@gmail.com>
2024-03-24 20:45:26 +0700

    Refactor code GPT component

--- Dashboard/src/ChatGPT.tsx
+++ Dashboard/src/ChatGPT.tsx
@@ -1,17 +1,16 @@
-import { useState }  from 'react';
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-  TypingIndicator,
-} from '@chatscope/chat-ui-kit-react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
 
-const API_KEY ="sk-WhMogOrZdMDghRtWpoSXT3BlbkFJZg9668YSkFJAVIKENMH"
+const API_KEY = "GPT_API_KEY";
 
-const ChatGPT = () => {
+interface Props {
+  temp: number;
+  humid: number;
+}
+
+const ChatGPT = (props: Props) => {
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm ChatGPT! Ask me anything!",
@@ -21,14 +20,14 @@ const ChatGPT = () => {
   ]);
   const [isTyping, setIsTyping] = useState(false);
 
-  const handleSendRequest = async (message) => {
+  const handleSendRequest = async (message: string) => {
     const newMessage = {
       message,
-      direction: 'outgoing',
+      direction: "outgoing",
       sender: "user",
     };
 
-    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setMessages((prevMessages: any[]) => [...prevMessages, newMessage]);
     setIsTyping(true);
 
     try {
@@ -39,7 +38,7 @@ const ChatGPT = () => {
           message: content,
           sender: "ChatGPT",
         };
-        setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
+        setMessages((prevMessages: any[]) => [...prevMessages, chatGPTResponse]);
       }
     } catch (error) {
       console.error("Error processing message:", error);
@@ -48,24 +47,21 @@ const ChatGPT = () => {
     }
   };
 
-  async function processMessageToChatGPT(chatMessages) {
+  async function processMessageToChatGPT(chatMessages: any[]) {
     const apiMessages = chatMessages.map((messageObject) => {
       const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
       return { role, content: messageObject.message };
     });
 
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        { role: "system", content: "I'm a Student using ChatGPT for learning" },
-        ...apiMessages,
-      ],
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "system", content: "I'm a Student using ChatGPT for learning" }, ...apiMessages],
     };
 
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
-        "Authorization": "Bearer " + API_KEY,
+        Authorization: "Bearer " + API_KEY,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(apiRequestBody),
@@ -76,24 +72,24 @@ const ChatGPT = () => {
 
   return (
     <div className="App">
-      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
         <MainContainer>
-          <ChatContainer>       
-            <MessageList 
-              scrollBehavior="smooth" 
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
               typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
             >
-              {messages.map((message, i) => {
-                console.log(message)
-                return <Message key={i} model={message} />
+              {messages.map((message: any, i: number) => {
+                console.log(message);
+                return <Message key={i} model={message} />;
               })}
             </MessageList>
-            <MessageInput placeholder="Send a Message" onSend={handleSendRequest} />        
+            <MessageInput placeholder="Send a Message" value={`Is ${props.temp} Celsius degree and ${props.humid} humidity percent good for health?`} onSend={handleSendRequest} />
           </ChatContainer>
         </MainContainer>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatGPT
+export default ChatGPT;
